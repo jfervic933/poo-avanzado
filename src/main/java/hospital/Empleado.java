f5aa135a88af34bd5eb9e344ab4e5a70c84db637
JCarlosVico <jfervic933@maralboran.es>
2020-03-26 11:49:51 +0100

    Ejercicio Hospital

---
+++ src/main/java/hospital/Empleado.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+/**
+ *
+ * @author carlos
+ */
+public class Empleado extends Persona{
+    
+    private String numeroSS;
+    private double salario;
+
+    public Empleado(String numeroSS, double salario, String nombre, String apellidos, Nif nif) {
+        super(nombre, apellidos, nif);
+        this.numeroSS = numeroSS;
+        this.salario = salario;
+    }
+
+    public String getNumeroSS() {
+        return numeroSS;
+    }
+
+    public void setNumeroSS(String numeroSS) {
+        this.numeroSS = numeroSS;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public double calcularIRPF(){
+        return salario*0.12;
+    }
+    
+    
+}
---
+++ src/main/java/hospital/GestionHospital.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+/**
+ *
+ * @author carlos
+ */
+public class GestionHospital {
+    
+    private Hospital hospital;
+
+    public GestionHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
+    
+    public static void main(String[] args) {
+        
+        // Se crea el hospital
+        
+        // Se da la opción de crear pacientes, crear médicos o administrativos
+        
+        // Ingresar pacientes y añadir administrativos, médicos al hospital
+        
+        // Recorremos la lista completa de empleados
+        
+            //  Llamamos a calcularIRPF de cada empleado
+            
+            // Si es un médico trate a un paciente y si es un administrativo registre un documento
+        
+            // Indicar donde hay conversiones explícitas e implícitas de tipos de datos
+    }
+    
+    public static Paciente crearPaciente(){
+        // Scanner preguntamos los datos del paciente
+    }
+    
+    
+    
+}
